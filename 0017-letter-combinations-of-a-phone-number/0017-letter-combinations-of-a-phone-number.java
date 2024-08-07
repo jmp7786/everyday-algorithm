dc844bbb47843774cfe3b9e81a35a1df6d8a7523
JMP <JMP7786@gmail.com>
2024-08-08 01:38:19 +0900

    Time: 1 ms (66.57%), Space: 42.4 MB (18.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,12 +1,10 @@
-import java.util.*;
-
 class Solution {
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) {
             return new ArrayList<>();
         }
-
         Map<Character, char[]> map = new HashMap<>();
+        map.put('1', new char[]{});
         map.put('2', new char[]{'a', 'b', 'c'});
         map.put('3', new char[]{'d', 'e', 'f'});
         map.put('4', new char[]{'g', 'h', 'i'});
@@ -15,25 +13,36 @@ class Solution {
         map.put('7', new char[]{'p', 'q', 'r', 's'});
         map.put('8', new char[]{'t', 'u', 'v'});
         map.put('9', new char[]{'w', 'x', 'y', 'z'});
-
-        List<String> result = new ArrayList<>();
-        backtrack(result, new StringBuilder(), digits, map, 0);
+        
+        List<String> result =  addChars(digits, map, 0);
         return result;
     }
 
-    private void backtrack(List<String> result, StringBuilder current, String digits, Map<Character, char[]> map, int index) {
-        if (index == digits.length()) {
-            result.add(current.toString());
-            return;
+    private List<String> addChars(String digits, Map<Character, char[]> map, int idx) {
+        List<String> result = new LinkedList<>();
+
+        if (idx == digits.length()-1)  {
+            char[] chars = map.get(digits.charAt(idx));
+            for(int i = 0; i < chars.length; i++) {
+                result.add(String.valueOf(chars[i]));
+            }
+            return result;
         }
 
-        char digit = digits.charAt(index);
-        char[] chars = map.get(digit);
-        for (char c : chars) {
-            current.append(c);
-            backtrack(result, current, digits, map, index + 1);
-            current.deleteCharAt(current.length() - 1);
+        
+        char[] chars = map.get(digits.charAt(idx));
+        
+
+        for(int i = 0; i < chars.length; i++) {
+            List<String> strs = addChars(digits, map, idx+1);
+            for(String str: strs) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(chars[i]);   
+                sb.append(str);   
+                result.add(sb.toString());
+            }
         }
+
+        return result;
     }
-
 }
