503603d760f045a93ed6d06c16f7a55ef514ee40
JMP <JMP7786@gmail.com>
2024-08-09 20:54:43 +0900

    Time: 2 ms (55.98%), Space: 44.5 MB (9.62%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        List<Character> path = new ArrayList<>();
+        path.add('(');
+        generate(1, 0, n, path, result);        
+        return result;
+    }
+
+    private void generate(int left, int right, int n, List<Character> path, List<String> result) {
+        if (left + right == n * 2) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < path.size(); i++) {
+                sb.append(path.get(i));
+            }
+            result.add(sb.toString());
+            return; 
+        }
+
+        if (left < n) {
+            path.add('(');
+            generate(left + 1, right, n, path, result);
+            path.removeLast();
+        }
+
+        if (right < left) {
+            path.add(')');
+            generate(left, right + 1, n, path, result);
+            path.removeLast();
+        }
+    }
+}
