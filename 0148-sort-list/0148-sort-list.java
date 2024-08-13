4e9fd4d2f57a8fb7cbda215a9cb945518fc3f216
JMP <JMP7786@gmail.com>
2024-08-13 13:28:02 +0900

    Time: 9 ms (92.18%), Space: 57.1 MB (22.22%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -14,12 +14,12 @@ class Solution {
             if (list1.val < list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
-                tail = tail.next;
             } else {
                 tail.next = list2;
                 list2 = list2.next;
-                tail = tail.next;
+                
             }
+            tail = tail.next;
         }
         tail.next = (list1 != null) ? list1 : list2;
         return dummyHead.next;
