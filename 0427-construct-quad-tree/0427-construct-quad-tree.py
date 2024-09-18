bb187f5b5f0c75b9352774542b0b5927ee6d030f
JMP <JMP7786@gmail.com>
2024-09-19 08:52:41 +0900

    Time: 85 ms (82.4%), Space: 17.3 MB (59.33%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -12,7 +12,6 @@ class Node:
 
 class Solution:
     def is_leaf(self, grid: List[List[int]], i: int, j: int, size:int) -> bool:
-        print(i, j, size)
         curr = grid[i][j]
         for x in range(i, i + size): 
             for y in range(j, j+ size): 
@@ -24,24 +23,21 @@ class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':   
         i =  j = 0
         size = len(grid)
-        node = self.construct_node(grid, i, j, size)
-        print(node)
+        node = self.build(grid, i, j, size)
         return node
 
-    def construct_node(self, grid: List[List[int]], i: int, j: int, size: int) -> 'Node':
+    def build(self, grid: List[List[int]], i: int, j: int, size: int) -> 'Node':
         is_leaf = self.is_leaf(grid, i, j, size)
-        print(is_leaf)
         if is_leaf: 
-            # grid[i][j]
             node = Node(grid[i][j], True)
             return node
         
         node = Node(False, False)
         half = size // 2
-        node.topLeft = self.construct_node(grid, i, j , half)
-        node.topRight = self.construct_node(grid, i, j  + half, half)
-        node.bottomLeft = self.construct_node(grid, i + half, j , half)
-        node.bottomRight = self.construct_node(grid, i + half, j + half , half)
+        node.topLeft = self.build(grid, i, j , half)
+        node.topRight = self.build(grid, i, j  + half, half)
+        node.bottomLeft = self.build(grid, i + half, j , half)
+        node.bottomRight = self.build(grid, i + half, j + half , half)
         return node
         
 
