2febe4ee22ca8a5cf7d78134d80824ece3a03853
JMP <JMP7786@gmail.com>
2024-09-18 23:23:30 +0900

    Time: 75 ms (59.66%), Space: 17.9 MB (43.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,48 +1,44 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans_left = ans_right = -1
         if len(nums) == 0: 
-            return [-1, -1]
-
-        ans_left = ans_right = -1 
-
+            return ans_left, ans_right
         
+        # find target left end
         if nums[0] == target: 
             ans_left = 0
         else: 
-            left = 0
-            right = len(nums)-1
+            left, right = 0, len(nums)-1
             while left <= right: 
                 mid = left + (right - left) // 2 
-                if nums[mid] == target and mid > 0 and  nums[mid-1] < nums[mid]: 
+                
+                if nums[mid] == target and mid > 0 and nums[mid-1] < nums[mid]: 
                     ans_left = mid
                     break
+
                 if nums[mid] >= target: 
-                    right = mid -1
-                else:
-                    left = mid +1
+                    right = mid - 1
+                else: 
+                    left = mid + 1                
 
-
+        # find target right end
         if nums[-1] == target: 
             ans_right = len(nums)-1
-        else:            
-            left = 0
-            right = len(nums)-1
+        else:
+            left, right = 0, len(nums)-1
             while left <= right: 
                 mid = left + (right - left) // 2 
                 
                 if nums[mid] == target and mid < len(nums)-1 and nums[mid+1] > nums[mid]: 
                     ans_right = mid
                     break
-                if nums[mid] <= target: 
-                    left = mid +1
-                else:
-                    right = mid -1
-        
 
+                if nums[mid] <= target: 
+                    left = mid + 1
+                else: 
+                    right = mid - 1                
 
         return ans_left, ans_right
-
-
             
             
             
