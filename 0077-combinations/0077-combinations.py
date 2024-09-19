ef3a450fcc5ab8c875284d6e15e7feda603d7af7
JMP <JMP7786@gmail.com>
2024-09-19 09:47:38 +0900

    Time: 575 ms (98.27%), Space: 37.3 MB (99.45%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,3 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-
-        def dfs(path: List[int], start: int): 
-            if k < len(path): 
-                return 
-            if k == len(path): 
-                result.append(path[:])
-                return 
-            
-            for i in range(start, n+1): 
-                path.append(i)
-                dfs(path, i+1)
-                path.pop()
-        
-        dfs([], 1)
-        
-        return result
+        return itertools.combinations([i for i in range(1, n+1)], k)
