9d1935c193b4e7918099e929bff54de99e7eaa86
JMP <JMP7786@gmail.com>
2024-09-19 09:52:46 +0900

    Time: 709 ms (53.15%), Space: 64.8 MB (22.53%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,3 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return itertools.combinations([i for i in range(1, n+1)], k)
+        nums = [i for i in range(1, n+1)]
+        result = []
+        self.build(0, nums, k, [], result)
+        return result
+
+
+    def build(self, start, nums, k, path, result): 
+        if len(path) == k:
+            result.append(path[:])
+            return 
+
+        for i in range(start, len(nums)): 
+            n = nums[i]
+            path.append(n)
+            self.build(i+1, nums, k, path, result)
+            path.pop()
+        
