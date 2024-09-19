3b7a48bea46d6c1ffaa6f1972006882ba587b118
JMP <JMP7786@gmail.com>
2024-09-19 09:23:29 +0900

    Time: 44 ms (16.74%), Space: 16.9 MB (11.35%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.build(n-1, n, '(', result)
+        return result
+    
+    def build(self, on, off, string, result): 
+        if on < -1 or off < -1:
+            return 
+        if on == 0 and off == 0 : 
+            result.append(string)
+            return 
+        
+        
+        if on <= off: 
+            self.build(on, off-1,  string+ ')', result)
+            self.build(on-1, off,  string+ '(', result)
+        else: 
+            self.build(on, off-1,  string+ ')', result)
+
+            
