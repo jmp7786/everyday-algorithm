777cb1d7a10f31192ba1fe5332594673982657ef
JMP <JMP7786@gmail.com>
2024-09-30 01:47:21 +0900

    Time: 49 ms (5.75%), Space: 17.6 MB (9.73%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,53 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: 
+            return None
+    
+        origin_memo = []
+        memo = []
+        node_map = {}
+        node_set = set()
+        prev = None
+        curr = head
+        while curr: 
+            node = Node(curr.val)
+            if curr.random:
+                node.random = curr.random
+                if curr.random not in node_map.keys(): 
+                    node_map[curr.random] = [node]
+
+            memo.append(node)
+            origin_memo.append(curr)
+            curr = curr.next
+
+        curr = head
+        # while curr: 
+        #     node = Node(curr.val)
+        #     if curr.random:
+        #         node.random = curr.random
+        #         if curr.random not in node_map.keys(): 
+        #             node_map[curr.random].append[node]
+
+        #     memo.append(node)
+        #     curr = curr.next
+
+        x = [(k, v) for k, v in node_map.items()]
+        print(x)
+        for i in range(len(memo)): 
+            if memo[i].random:
+                memo[i].random = memo[origin_memo.index(memo[i].random)]
+
+        for i in range(1, len(memo)): 
+            memo[i-1].next = memo[i]
+        
+        return memo[0]
+            
