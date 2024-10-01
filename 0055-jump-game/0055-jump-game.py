ce74459a65a730f86e84749817266c3961a205a8
JMP <JMP7786@gmail.com>
2024-10-01 22:52:42 +0900

    Time: 5892 ms (5.62%), Space: 18.1 MB (8.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,8 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        reach = 0
-        i = 0 
-        while i <= reach and i < len(nums): 
-            reach = max(reach, i+nums[i])
-            i +=1
-        return reach >= len(nums)-1
+        n = len(nums)
+        memo = [float('inf')] * n
+        memo[-1] = 0
+        for i in range(n-2, -1, -1): 
+            count = nums[i]
+            jump = 1 if count else float('inf')
+            memo[i] = jump + min( memo[i+1:i+1+count] if memo[i+1:i+1+count] else [0] )
+        
+        return memo[0] != float('inf')
+            
