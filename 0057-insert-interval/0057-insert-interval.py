1d89f13727558a0e7626477ff29d5ed6a8b3da9a
JMP <JMP7786@gmail.com>
2024-10-01 14:44:49 +0900

    Time: 303 ms (5.38%), Space: 20.3 MB (5.69%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,40 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals: 
+            return [newInterval]
+        new_start, new_end = newInterval
+
+        candidates = [] 
+        for i in range(len(intervals)): 
+            start, end = intervals[i]
+
+            if new_end >= end and new_start <= end : 
+                print('new_start <= end', new_start,end)
+                candidates.append(i)
+                new_start = min(new_start, start)
+            elif new_start <= start and new_end >= start:
+                print('new_end >= start', new_end , start)
+                candidates.append(i)
+                new_end = max(new_end, end)
+            elif start<= new_start and new_end <=end: 
+                candidates.append(i)
+                new_start = min(new_start, start)
+                new_end = max(new_end, end)
+        
+        merged_interval = [new_start, new_end]
+        print('caididates', candidates)
+        print('new_merged_interval', merged_interval)
+        new_intervals = []
+        inserted = False
+        for i in range(len(intervals)): 
+            if i not in candidates:
+                new_intervals.append(intervals[i])
+            elif not inserted: 
+                inserted= True
+                new_intervals.append(merged_interval)
+        # merge는 되지 않고 interval만 있을 경우                 
+        if not inserted:
+            intervals.append(newInterval)
+            intervals.sort()
+            new_intervals = intervals
+        return new_intervals
