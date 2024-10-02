b04317357c1aaeb11e4c883bc98f8fed173cd956
JMP <JMP7786@gmail.com>
2024-10-02 13:26:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[float('inf')] * (amount+1) for _ in range(len(coins)+1)]
-        n = len(coins)
-        for i in range(n+1): 
-            dp[i][0] = 0
-        for i in range(1, amount+1): 
-            minumum = float('inf')
-            for j in range(len(coins)): 
-                coin = coins[j]
-                if i-coin >=0: 
-                    dp[j][i]  =  dp[n][i-coin]+1
-                    minumum = min(minumum, dp[j][i])
-                
-            dp[n][i] = minumum
-
-        return dp[n][amount] if dp[n][amount] != float('inf') else -1
+        memo = [[float('inf')] * (len(coins)+1) for _ in range(amount+1)]
+        memo[0] = [0] * (len(coins)+1)
+        for n in range(1, amount+1): 
+            for i in range(len(coins)): 
+                c = coins[i]
+                if n - c >= 0: 
+                    if n-c >= 0:
+                        memo[n][i] =+ memo[n-c][len(coins)]
+                    memo[n][i] += 1
+            
+            memo[n][len(coins)] = min(memo[n]) 
+        print(memo)
+        result = memo[-1][len(coins)]
+        return  result if result != float('inf') else -1
+        # return 1 
