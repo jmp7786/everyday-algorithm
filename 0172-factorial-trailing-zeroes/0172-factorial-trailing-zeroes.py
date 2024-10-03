ffb4379e69fece855a7f7492cb509683d920e384
JMP <JMP7786@gmail.com>
2024-10-03 17:29:09 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        n = math.factorial(n)
+        count = 0
+        while n % 10 == 0:
+            print(count)
+            n  = n // 10
+            print(n)
+            count +=1
+
+        
+        return count
