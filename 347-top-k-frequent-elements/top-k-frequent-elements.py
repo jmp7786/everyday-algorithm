ede2aeb30fd953c87599dc064f53e27377183810
Jaemin Park <14803858+jmp7786@users.noreply.github.com>
2026-01-26 10:39:05 +0900

    Time: 15 ms (11.01%) | Memory: 24.8 MB (10.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -11,25 +11,17 @@ class Solution:
         for num in nums:
             counter[num] += 1
         
-        bucket = [None] * (len(nums) + 1)
+        bucket = [[] for _ in range(len(nums) + 1)]
         for num, freq in counter.items():
-            if bucket[freq] is None:
-                bucket[freq] = list()
             bucket[freq].append(num)
         
         result = []
         limit = 0
-        for i in range(len(bucket) -1, -1, -1):
+        for i in range(len(bucket) -1, 0, -1):
             sub_list = bucket[i]
-            if sub_list is not None:
-                for num in sub_list:
-                    if limit < k:
-                        result.append(num)
-                        limit += 1
-                    else:
-                        break
-            
-            if limit == k:
-                break
-        
-        return result
+            for num in sub_list:
+                result.append(num)
+                limit += 1
+                if limit == k:
+                    return result
+                    
