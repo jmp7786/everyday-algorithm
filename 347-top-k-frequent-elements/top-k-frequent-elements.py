c2e426b5473b6cdb798d6e656ce273b4d6ace8c2
Jaemin Park <14803858+jmp7786@users.noreply.github.com>
2026-01-26 09:45:53 +0900

    Time: 7 ms (54.34%) | Memory: 23.1 MB (25.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,23 +4,20 @@ from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # 1. counting frequency for all num 
-        # 2. create a heap forl;kj
+        # 2. create a heap for
         # 3. iterate map for insert to min heap 
         # 4. pop k time for return values 
         counter = defaultdict(int)
         for i in range(len(nums)):
             counter[nums[i]] += 1
         
-        heap = []
-        limit = 0
+        min_heap = []
         for key, value in counter.items():
-            heapq.heappush(heap, (-value, key))
-        
-        result = []
-        for _ in range(k):
-            result.append(heapq.heappop(heap)[1])
+            heapq.heappush(min_heap, (value, key))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
          
-        return result
+        return [key for value, key in min_heap]
             
 
 
