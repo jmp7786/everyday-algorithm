73c4028dccc18ff2379372a90a687dfd31769a46
Jaemin Park <14803858+jmp7786@users.noreply.github.com>
2026-01-26 09:08:32 +0900

    Time: 6 ms (58.55%) | Memory: 22.9 MB (31.80%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -7,16 +7,19 @@ class Solution:
         # 2. create a heap forl;kj
         # 3. iterate map for insert to min heap 
         # 4. pop k time for return values 
-
         counter = defaultdict(int)
         for i in range(len(nums)):
             counter[nums[i]] += 1
         
-        min_heap = [(value, key) for key, value in counter.items()]
-        heapq.heapify(min_heap)
-
-        top_k = heapq.nlargest(k, min_heap)
-        result = [tupl[1] for tupl in top_k ]
+        heap = []
+        limit = 0
+        for key, value in counter.items():
+            heapq.heappush(heap, (-value, key))
+        
+        result = []
+        for _ in range(k):
+            result.append(heapq.heappop(heap)[1])
+         
         return result
             
 
