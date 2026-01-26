0cd9a028f5451fb4c0814d45b97c8275c810fb02
Jaemin Park <14803858+jmp7786@users.noreply.github.com>
2026-01-26 10:42:19 +0900

    Time: 13 ms (13.84%) | Memory: 25 MB (5.31%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -11,17 +11,15 @@ class Solution:
         for num in nums:
             counter[num] += 1
         
-        bucket = [[] for _ in range(len(nums) + 1)]
+        buckets = [[] for _ in range(len(nums) + 1)]
         for num, freq in counter.items():
-            bucket[freq].append(num)
+            buckets[freq].append(num)
         
         result = []
-        limit = 0
-        for i in range(len(bucket) -1, 0, -1):
-            sub_list = bucket[i]
+        for i in range(len(buckets) -1, 0, -1):
+            sub_list = buckets[i]
             for num in sub_list:
                 result.append(num)
-                limit += 1
-                if limit == k:
+                if len(result) == k:
                     return result
                     
