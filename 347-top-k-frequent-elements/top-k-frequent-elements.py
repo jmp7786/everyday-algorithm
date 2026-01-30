9f0f66c340b2f04af7bdc1da521af7b02ccd9e34
Jaemin Park <14803858+jmp7786@users.noreply.github.com>
2026-01-30 09:36:29 +0900

    Time: 7 ms (54.45%) | Memory: 22.9 MB (34.56%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,25 +1,42 @@
-from collections import defaultdict
+from collections import Counter
+import random 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # lets make a buket sorting 
-        # 1. scatter this 
-        # 2. sort each buckets
-        # 3. gather all buckets
+        freq_map = Counter(nums)
+        uniques = list(freq_map.keys())
+        slice_idx = self._quick_select(uniques, freq_map, 0, len(uniques)-1, len(uniques)-k)
+        return uniques[slice_idx:]
+
+    def _quick_select(self, nums: List[int], freq_map: dict, left: int, right: int, k_smallest: int) -> int:
+        if left >= right:
+            return right
         
-        counter = defaultdict(int)
-        for num in nums:
-            counter[num] += 1
+        pivot_idx = random.randint(left, right)
+        new_pivot_idx = self._partition(nums, freq_map, left, right, pivot_idx)
+        if new_pivot_idx == k_smallest:
+            return new_pivot_idx
+        elif new_pivot_idx > k_smallest:
+            return self._quick_select(nums, freq_map, left, new_pivot_idx-1, k_smallest)
+        else:
+            return self._quick_select(nums, freq_map, new_pivot_idx+1, right, k_smallest)
+
         
-        buckets = [[] for _ in range(len(nums) + 1)]
-        for num, freq in counter.items():
-            buckets[freq].append(num)
+
+    def _partition(self, nums: List[int], freq_map: dict, left: int, right: int, pivot_idx: int) -> int:
+        freq = freq_map[nums[pivot_idx]]
+
+        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+
+        store_idx = left
         
-        result = []
-        for i in range(len(buckets) -1, 0, -1):
-            sub_list = buckets[i]
-            for num in sub_list:
-                result.append(num)
-                if len(result) == k:
-                    return result
-                    
+        for i in range(left, right):
+            if freq >= freq_map[nums[i]]:
+                nums[i], nums[store_idx] = nums[store_idx], nums[i]
+                store_idx +=1
+        
+        nums[right], nums[store_idx] = nums[store_idx], nums[right]
+        
+        return store_idx
+
+        
